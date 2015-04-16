855e9d20cbcf014c541534580b1da6f392c7babf
Ripta Pasay <github@r8y.org>
2015-04-16 13:20:04 -0700

    Farm animals

--- farm/lib/farm.rb
+++ farm/lib/farm.rb
@@ -1,5 +1,22 @@
 using Article
 
+# Null object in h-myah.
+class SilentAnimal
+  def sound
+    '<silence>'
+  end
+
+  def species
+    '<silence>'
+  end
+end
+
+class AnimalFactory
+  def self.for(animal)
+    animal || SilentAnimal.new
+  end
+end
+
 class Farm
   attr_reader :animals
 
@@ -8,18 +25,18 @@ class Farm
   end
 
   def lyrics
-    animals.collect {|animal| verse(animal) }.join("\n\n")
+    animals.collect { |animal| verse(AnimalFactory.for(animal)) }.join("\n\n")
   end
 
   def verse(animal)
     sound   = animal.sound
     species = animal.species
     "Old MacDonald had a farm, E-I-E-I-O,\n" +
-    "And on that farm he had #{species.articlize}, E-I-E-I-O,\n" +
-    "With #{sound.articlize} #{sound} here " +
-      "and #{sound.articlize} #{sound} there,\n" +
-    "Here #{sound.articlize}, there #{sound.articlize}, " +
-      "everywhere #{sound.articlize} #{sound},\n" +
-    "Old MacDonald had a farm, E-I-E-I-O."
+      "And on that farm he had #{species.articlize}, E-I-E-I-O,\n" +
+      "With #{sound.articlize} #{sound} here " +
+        "and #{sound.articlize} #{sound} there,\n" +
+      "Here #{sound.articlize}, there #{sound.articlize}, " +
+        "everywhere #{sound.articlize} #{sound},\n" +
+      "Old MacDonald had a farm, E-I-E-I-O."
   end
 end
